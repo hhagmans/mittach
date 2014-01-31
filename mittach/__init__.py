4c22c76228ebecaf0c3c6b49c003254a5a79dfcf
hhagmans <hendrik.hagmans@web.de>
2014-01-31 16:36:06 +0100

    Added Dependencies to setup.py again

--- mittach/__init__.py
+++ mittach/__init__.py
@@ -4,5 +4,5 @@ innoQ lunch planning and registration
 https://github.com/innoq/mittach
 """
 
-__version__ = "0.7.6"
+__version__ = "0.7.7"
 # TODO: author, contributors, copyright, license and all that nonsense
